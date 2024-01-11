3e7404c89368723c739930bead0b90efd1d88480
marcelldac <dacmarcell@gmail.com>
2024-01-11 13:55:06 -0300

    feat: entry data component

---
+++ src/app/entry-data/entry-data.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-entry-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './entry-data.component.html',
+  styleUrl: './entry-data.component.css',
+})
+export class EntryDataComponent {}
