67fb09ab5c86fb4b0bec296b427ba7e6bc1c4523
marcelldac <dacmarcell@gmail.com>
2024-01-11 12:55:14 -0300

    style: lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
