587eac2ad26f44073a479e441eb4435c08b29227
marcelldac <dacmarcell@gmail.com>
2024-01-11 12:44:40 -0300

    feat: input father component in input placeholder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-
+  buttonText: string = 'Acessar';
+  buttonNumber: number = 10;
+  buttonArray: string[] = ['Comprar', 'Vender'];
+  buttonObject = {
+    label: 'Comprar',
+  };
+  @Input() placeholder: string = '';
 }
