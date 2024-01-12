5230f68880b8fea72bdde70120631ad9d54042a1
marcelldac <dacmarcell@gmail.com>
2024-01-12 00:09:40 -0300

    feat: import and call entry data and using pipes components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { EntryDataComponent } from './entry-data/entry-data.component';
+import { UsingPipesComponent } from './using-pipes/using-pipes.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroesComponent,
+    EntryDataComponent,
+    UsingPipesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
