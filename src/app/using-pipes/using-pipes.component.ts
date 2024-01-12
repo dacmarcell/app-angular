e0226f80e8a94427da9093daceace3cff0f374ba
marcelldac <dacmarcell@gmail.com>
2024-01-12 00:08:43 -0300

    feat: using pipes component

---
+++ src/app/using-pipes/using-pipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-using-pipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './using-pipes.component.html',
+  styleUrl: './using-pipes.component.css'
+})
+export class UsingPipesComponent {
+
+}
